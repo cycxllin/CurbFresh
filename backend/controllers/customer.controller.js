9e1872c5b1f68ebf97165a2dd9fdaf4a979c0a96
cycxllin <cycxllin@gmail.com>
2024-03-04 13:27:46 -0700

    changed customer phone to unique, added optional email

--- backend/controllers/customer.controller.js
+++ backend/controllers/customer.controller.js
@@ -1,17 +1,15 @@
-import Customer from '../models/customer.model.js'
 import { getCustomerByPhoneFromRepo, addCustomerToRepo } from "../repositories/customer.repository.js";
 
 export const getCustomerByPhone = async (req, res, next) => {
     try {
-        const phone = (req.params);
-        console.log("first: " + phone);
+        const phone = req.params.phone;
         const customer = await getCustomerByPhoneFromRepo(phone);
 
         if (customer) {
             return res.status(200).json({
                 status: 200,
                 message: 'retrieved customer sucessfully',
-                data: student
+                data: customer
             });
         } else {
             return res.status(404).json({
@@ -29,13 +27,18 @@ export const getCustomerByPhone = async (req, res, next) => {
 
 export const addCustomer = async (req, res, next) => {
     try {
-        //generate sequential custID
-        const customerID = 0; //placeholder --> chanfge ot generate sequential id
-        const customer = {
-            _id: customerID,
-            ...req.body
-        };
-    
+        let customer = {};
+
+        if (req.body.email) {
+            customer = {
+                ...req.body
+            };
+        } else {
+            customer = {
+                ...req.body
+            };
+        }
+
         const addedCustomer = await addCustomerToRepo(customer);
 
         if (addedCustomer) {
--- backend/repositories/customer.repository.js
+++ backend/repositories/customer.repository.js
@@ -2,11 +2,10 @@ import Customer from '../models/customer.model.js';
 
 export const getCustomerByPhoneFromRepo = async (phone) => {
     try {
-        console.log(phone.phone);
-        const customer = await Customer.findOne(phone.phone);
+        const customer = await Customer.findOne({phone:phone});
         return customer;
     } catch (error) {
-        throw Error(`Error while retrieving customer with phone: ${phone.phone}`);
+        throw Error(`Error while retrieving customer with phone: ${phone}`);
     }
 };
 
