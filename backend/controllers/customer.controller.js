d08515868a8afd6c1f5e465c2fc4a46fddff6b0d
cycxllin <cycxllin@gmail.com>
2024-03-04 15:47:43 -0700

    addCustomer checks if customer is inactive

--- backend/controllers/customer.controller.js
+++ backend/controllers/customer.controller.js
@@ -1,4 +1,5 @@
-import { getCustomerByPhoneFromRepo, addCustomerToRepo, updateCustomerInRepository, deleteCustomerFromRepository } from "../repositories/customer.repository.js";
+import { getCustomerByPhoneFromRepo, addCustomerToRepo, updateCustomerInRepository, 
+    deleteCustomerFromRepository, getCustomersFromRepository } from "../repositories/customer.repository.js";
 
 export const getCustomerByPhone = async (req, res, next) => {
     try {
@@ -25,27 +26,53 @@ export const getCustomerByPhone = async (req, res, next) => {
     }
 };
 
+//checks if customer was previously deleted, updates if customer exists
 export const addCustomer = async (req, res, next) => {
     try {
-        let customer = {};
+        const { body } = req;
 
-        if (req.body.email) {
+        let customer = await getCustomerByPhoneFromRepo(body.phone);
+        let addedCustomer = {};
+
+        if (customer) {
+            // customer exists and was previously deleted
+            if (customer.active === false) {
+                            // set active and update info
             customer = {
-                ...req.body,
+                ...body,
                 active: true
             };
+
+            addedCustomer = await updateCustomerInRepository({phone: body.phone}, customer);
+            }
+            else {
+                // customer exists but is still active so reject adding
+                return res.status(400).json({
+                    status: 400,
+                    message: `Customer with phone ${body.phone} already exists`
+                });
+            }
+
+
         } else {
-            customer = {
-                email: '',
-                ...req.body,
-                active: true
-            };
+            if (req.body.email) {
+                customer = {
+                    ...body,
+                    active: true
+                };
+            } else {
+                customer = {
+                    email: '',
+                    ...body,
+                    active: true
+                };
+            }
+    
+         addedCustomer = await addCustomerToRepo(customer);
         }
 
-        const addedCustomer = await addCustomerToRepo(customer);
-
         if (addedCustomer) {
-            return res.status(204).json({
+            return res.status(200).json({
                 status: 200,
                 message: 'added customer sucessfully',
                 data: addedCustomer
@@ -74,8 +101,8 @@ export const deleteCustomer = async function (req, res) {
         const customer = await deleteCustomerFromRepository({phone: phone});
 
         if (customer){
-            return res.status(204).json({
-                status: 204,
+            return res.status(200).json({
+                status: 200,
                 message: `deleted customer successfully`,
                 data: customer
             });
@@ -110,5 +137,26 @@ export const updateCustomer = async function (req, res) {
         }
     } catch (error) {
         res.status(500).send(`failed to update customer`);
+    }
+}
+
+/* Get a LIST of active Customers */
+export const getCustomers = async function (req, res) {
+    try {
+        const customers = await getCustomersFromRepository({active: true});
+        if (customers) {
+            return res.status(200).json({
+                status: 200,
+                message: 'found customers sucessfully',
+                data: customers
+            });
+        } else {
+            return res.status(404).json({
+                status: 404,
+                message: `Error finding customers`,
+            });
+        }
+    }catch (error) {
+        res.status(500).send(`failed to get customers`);
     }
 }
