095ab79d0e3a799b194f0040311c483f251373a3
ormle <101660382+ormle@users.noreply.github.com>
2024-03-04 15:32:32 -0700

    comments

--- backend/routes/restaurant.route.js
+++ backend/routes/restaurant.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addRestaurant, getRestaurant, getRestaurants, updateRestaurant, deleteRestaurant} from "../controllers/restaurant.controller.js";
+import { addRestaurant, getRestaurant, getRestaurants, updateRestaurant, deleteRestaurant, getRestaurantSaleInfo, getRestaurantMenuPopularity, getRestaurantBusiestTime} from "../controllers/restaurant.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +8,10 @@ router.get("/", getRestaurants);          //List of Restaurants
 router.get("/:_id", getRestaurant);       //Singular Restaurant
 router.patch("/:_id", updateRestaurant);  //Update Restaurant Info
 router.delete("/:_id", deleteRestaurant); //Delete Restaurant
+
+//Restaurant Analytics
+router.get("/analytics/sales/:_id", getRestaurantSaleInfo);         //Get Sale info
+router.get("/analytics/popular/:_id", getRestaurantMenuPopularity); //List popular menu items in order
+router.get("/analytics/busy/:_id", getRestaurantBusiestTime);       //Returns a list of the times where most orders are picked up (hourly)
 
 export default router;
